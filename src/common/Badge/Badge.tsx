f86ca49242974fedf2a7c50cb742b5cc1177cefd
Rue <ruesullivanpro@gmail.com>
2021-02-07 15:51:48 +0400

    Typing Badge element

--- src/common/Badge/Badge.tsx
+++ src/common/Badge/Badge.tsx
@@ -3,33 +3,43 @@ import { colors } from '../constants'
 import { Text } from '../Text/Text'
 import { BadgeStyled } from './styles'
 
-export const Badge = (props: any) => {
+export enum BadgeTypeEnum {
+  approved,
+  inProgress,
+  inReview,
+  waiting
+}
+
+export interface IBadgeProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: string
+  type?: BadgeTypeEnum
+}
+
+export const Badge = ({ children, type }: IBadgeProps): React.ReactElement => {
   let color = 'black'
   let bgColor = 'black'
-
-  switch (props.children) {
-    case 'Approved':
+  switch (type) {
+    case BadgeTypeEnum.approved:
       color = colors.primary
       bgColor = colors.purple_light
       break
-    case 'In Progress':
+    case BadgeTypeEnum.inProgress:
       color = colors.green
       bgColor = colors.green_light
       break
-    case 'In Review':
+    case BadgeTypeEnum.inReview:
       color = colors.red
       bgColor = colors.red_light
       break
-    case 'Waiting':
+    case BadgeTypeEnum.waiting:
       color = colors.gray
       bgColor = colors.gray_light
       break
   }
-
   return (
-    (props.children && (
-      <BadgeStyled bgColor={bgColor} color={color} {...props}>
-        <Text color={color}>{props.children}</Text>
+    (children && (
+      <BadgeStyled bgColor={bgColor} color={color}>
+        <Text color={color}>{children}</Text>
       </BadgeStyled>
     )) || <></>
   )
